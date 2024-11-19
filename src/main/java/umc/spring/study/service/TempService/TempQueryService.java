232b163fe2853268fb83ea15de6b563e7fb6f2cd
chaeone02 <sandy12150831@gmail.com>
2024-11-19 14:10:08 +0900

    Feat(7주차): API응답/에러핸들러 실습 완료

---
+++ src/main/java/umc/spring/study/service/TempService/TempQueryService.java
@@ -0,0 +1,7 @@
+package umc.spring.study.service.TempService;
+
+public interface TempQueryService {
+
+    void CheckFlag(Integer flag);
+
+}
--- src/main/java/umc/spring/study/web/controller/TempRestController.java
+++ src/main/java/umc/spring/study/web/controller/TempRestController.java
@@ -4,16 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.study.apiPayload.ApiResponse;
 import umc.spring.study.converter.TempConverter;
+import umc.spring.study.service.TempService.TempQueryService;
 import umc.spring.study.web.dto.TempResponse;
+
 
 @RestController
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
 
+    private final TempQueryService tempQueryService;
+
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam(name = "flag") Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
